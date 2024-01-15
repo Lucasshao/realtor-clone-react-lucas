9a48e1e439fc21ff97f84512e6f545a2628745b9
Lucasshao <yilongshao2020@gmail.com>
2024-01-15 22:31:11 +1100

    install reactjs and tailwindcss and create the first template

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// always check files with above extension make the css related to the classes that is added
